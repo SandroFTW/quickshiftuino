8a3c3ee3745e9702bf1de17f0e8f9953919529e8
Sandro Hafner <34171311+SandroFTW@users.noreply.github.com>
2024-03-26 10:48:23 +0100

    Optimize Port Manipulation
    + add pictures

--- src/src.ino
+++ src/src.ino
@@ -1,52 +1,37 @@
-// Uncomment to use load cell instead of piezo sensor
-//#define LOADCELL
-
 // CHANGE CONSOLE DEBUG OUTPUT
-//#define DEBUG_SENSOR_PLOT
 //#define DEBUG_SENSOR
-  #define DEBUG_SHIFT
+//#define DEBUG_SENSOR_PLOT
+//#define DEBUG_SHIFT
 //#define DEBUG_RPM
-
-#ifdef LOADCELL
-  #include <HX711_ADC.h>
-  #include <EEPROM.h>
-
-  #define calAdressEEPROM 0
-  #define tareAdressEEPROM 4
-
-  #define HX711_DOUT 4 // MCU -> HX711 Dout Pin
-  #define HX711_SCK  5 // MCU -> HX711 SCK Pin
-#elif
-  #define PIEZO_SENSOR    19 // A5  (Piezo Sensor Pin)
-#endif
 
 // QUICKSHIFTER CONFIGURATION
 #define loopTime         5    // 200 Hz
 #define rpmLoopTime      200  // 5 Hz
 
-#define cutTimeMax       100  // 100 ms at minRPM
-#define cutTimeMin       85   // 85 ms at maxRPM
+#define cutTime1         60    // 3000 - 5375   RPM
+#define cutTime2         60    // 5376 - 7750   RPM  // War vorher 55
+#define cutTime3         55    // 7751 - 10125  RPM
+#define cutTime4         50    // 10126 - 12500 RPM
 
 #define minRPM           3000  // min. required RPM to allow QS 
-#define maxRPM           12000 // Engine RPM for minCutTime (still works above maxRPM, cutTime is always cutTimeMin)
+#define maxRPM           12500 // Engine RPM for minCutTime (still works above, always cutTimeMin)
+#define rpmStep          (maxRPM - minRPM) / 4
 
 #define delayTime        25   // 25 ms (delay after trigger force is reached)
 #define deadTime         300  // 300 ms (dead time before next cut)
 #define rpmFactor        60 * ((1000 / rpmLoopTime))
 
 // SENSITIVITY VALUES
-#ifdef LOADCELL
-  #define cutSensitivity   5000  // ~ 5000 g on the load cell
-#elif
-  #define cutSensitivity   200   // min. piezo force threshold
-#endif
+#define cutSensitivity   280   // min. piezo force threshold
 
 // PIN DEFINES
-#define CUT_SIG   10 // D10 (Ignition cut MOSFET pin)
-#define GREEN_LED 3  // D3
-#define RED_LED   2  // D2
+#define PIEZO_SENSOR     18 // A4  (Piezo Sensor Pin)
+#define CUT_SIG          10 // D10, PB2 (Ignition cut MOSFET pin)
+#define GREEN_LED        3  // D3
+//#define RED_LED          2  // D2
 
 // GLOBAL VARIABLES
+static unsigned long lastPrintMillis = 0; // Time of last Serial.print for Sensor value
 static unsigned long lastPulseMillis = 0; // Time of last RPM calculation
 static unsigned long lastCutMillis = 0;   // Time of last shiftQueued
 static unsigned long currMillis = 0;      // millis()
@@ -58,40 +43,45 @@ static bool shiftQueued = false;          // Is shiftQueued because threshold wa
 static float cutTime = 0;                 // Calculated cutTime based on RPM
 static float lastRPM = 0;                 // Last calculated RPM
 
-#ifdef LOADCELL
-  HX711_ADC LoadCell(HX711_DOUT, HX711_SCK);
-  static float sensorValue = 0;
-#elif
-  static int sensorValue = 0;
-#endif
+static int sensorValue = 0;
 
 // CALCULATE CUTOFF TIME BASED ON ENGINE RPM
 int cutoffTime()
 {
-  if (lastRPM > maxRPM)
+  if (lastRPM >= minRPM && lastRPM <= (minRPM + rpmStep))
   {
-    return cutTimeMin; // If RPM is above maxRPM always use cutTimeMin
+    return cutTime1;
   }
-  else
+  else if (lastRPM > (minRPM + rpmStep) && lastRPM <= (minRPM + 2*rpmStep))
   {
-    return map(lastRPM, minRPM, maxRPM, cutTimeMax, cutTimeMin); // Scales cutoffTime based on RPM (more RPM -> shorter cut)
+    return cutTime2;
+  }
+  else if (lastRPM > (minRPM + 2*rpmStep) && lastRPM <= (minRPM + 3*rpmStep))
+  {
+    return cutTime3;
+  }
+  else if (lastRPM > (minRPM + 3*rpmStep))
+  {
+    return cutTime4;
   }
 }
 
 void disable_ign()
 {
-  //PORTB |= (0 << 2); // Disable Ignition
-  //PORTD |= (1 << 3); // Enable Green LED
-  digitalWrite(CUT_SIG, LOW);
-  digitalWrite(GREEN_LED, HIGH);
+  PORTB &= ~(1 << 2);  // Disable Ignition
+  PORTD |= (1 << 3); // Enable Green LED
+
+  //digitalWrite(CUT_SIG, LOW);
+  //digitalWrite(GREEN_LED, HIGH);
 }
 
 void enable_ign()
 {
-  //PORTB |= (1 << 2); // Enable Ignition
-  //PORTD |= (0 << 3); // Disable Green LED
-  digitalWrite(CUT_SIG, HIGH);
-  digitalWrite(GREEN_LED, LOW);
+  PORTB |= (1 << 2); // Enable Ignition
+  PORTD &= ~(1 << 3)   // Disable Green LED
+
+  //digitalWrite(CUT_SIG, HIGH);
+  //digitalWrite(GREEN_LED, LOW);
 }
 
 void countPulse()
@@ -99,81 +89,34 @@ void countPulse()
   pulseCount++;
 }
 
-int updatePiezoValue()
-{
-  if (currMillis >= (lastMillis + loopTime))
-  {
-    sensorValue = analogRead(PIEZO_SENSOR) - 512;
-    lastMillis = currMillis;
-
-    #ifdef DEBUG_SENSOR
-      Serial.print(currMillis); Serial.print(" - sensorValue: "); Serial.println(sensorValue);
-    #endif
-
-    #ifdef DEBUG_SENSOR_PLOT
-      Serial.print("min:-512,max:512,sensorValue:"); Serial.println(sensorValue);
-    #endif
-  }
-}
-
-float updateLoadcellValue()
-{
-  if (LoadCell.update())
-  {
-    sensorValue = LoadCell.getData();
-    lastMillis = currMillis;
-
-    #ifdef DEBUG_SENSOR
-      Serial.print(currMillis); Serial.print(" - sensorValue: "); Serial.println(sensorValue);
-    #endif
-
-    #ifdef DEBUG_SENSOR_PLOT
-      Serial.print("min:-20000,max:20000,sensorValue:"); Serial.println(sensorValue);
-    #endif
-  }
-}
-
 
 
 void setup()
 {
-  pinMode(GREEN_LED,    OUTPUT);
+  //pinMode(GREEN_LED,    OUTPUT);
   //pinMode(RED_LED,      OUTPUT);
-  pinMode(CUT_SIG,      OUTPUT);
+  //pinMode(CUT_SIG,      OUTPUT);
+ 
+  //digitalWrite(GREEN_LED, LOW);
+  //digitalWrite(RED_LED,   LOW);
+  //digitalWrite(CUT_SIG,   HIGH);
 
-  digitalWrite(GREEN_LED, LOW);
-  //digitalWrite(RED_LED,   HIGH);
-  digitalWrite(CUT_SIG,   HIGH);
+  //pinMode(PIEZO_SENSOR, INPUT);
 
-  Serial.begin(57600);
+  DDRD |= (1 << DDD3) // D3 (Green LED) as Output
+  DDRB |= (1 << DDB2) // D10 (Ign Cut FET) as Output
 
-  #ifdef LOADCELL
-    LoadCell.begin();
+  PORTD &= ~(1 << DDD3) // Turn off Green LED
+  PORTB |= (1 << DDB2) // Turn on Ignition
 
-    float calibrationValue; // = 75.2 for gramms
-    long tareValue;
-    EEPROM.get(calAdressEEPROM, calibrationValue);
-    EEPROM.get(tareAdressEEPROM, tareValue);
+  DDRC &= ~(1 << DDC4) // A4 (Piezo) as Input
 
-    Serial.print("calValue: "); Serial.print(calibrationValue); Serial.print(" - tareValue: "); Serial.println(tareValue);
-
-    LoadCell.start(stabilizingTime, false); // Set to true to enable tare on power-up
-
-    if (LoadCell.getTareTimeoutFlag()) { Serial.println("MCU>HX711 timeout"); while (1); }
-    else { // HX711 connection successful
-      LoadCell.setCalFactor(calibrationValue);
-      PORTD |= (0 << 3); // Disable Green LED when startup is complete -> digitalWrite(GREEN_LED, LOW);
-    }
-  #elif
-    pinMode(PIEZO_SENSOR, INPUT);
-  #endif
+  Serial.begin(57600); // 115200
 
   // Interrupt routine for ingition pulse counting / RPM calculation
-  /*
   DDRD &= ~(1 << 2); // Set D2 as input pin
   PORTD |= (1 << 2); // Enable internal pullup for D2
   attachInterrupt(0, countPulse, RISING); // D2 as Interrupt
-  */
 }
 
 void loop()
@@ -182,19 +125,22 @@ void loop()
 
   if (currMillis >= (lastMillis + loopTime))
   {
-    #ifdef LOADCELL
-      sensorValue = updateLoadcellValue();
-    #elif
-      sensorValue = updatePiezoValue();
+    sensorValue = analogRead(PIEZO_SENSOR) - 512;
+    lastMillis = currMillis;
 
-    #ifdef DEBUG_SENSOR
-      Serial.print(currMillis); Serial.print(" - sensorValue: "); Serial.println(sensorValue);
-      //Serial.print("min:-512,max:512,sensorValue:"); Serial.println(sensorValue);            // Plot min/max for piezo sensorValue
-      //Serial.print("min:-20000,max:20000,sensorValue:"); Serial.println(sensorValue);        // Plot min/max for loadcell sensorValue
-    #endif
+    if (currMillis >= lastPrintMillis + 35)
+    {
+      #ifdef DEBUG_SENSOR
+        Serial.print(currMillis); Serial.print(" - sensorValue: "); Serial.println(sensorValue);
+      #endif
+
+      #ifdef DEBUG_SENSOR_PLOT
+        Serial.print("min:-512,max:512,sensorValue:"); Serial.println(sensorValue);
+      #endif
+
+      lastPrintMillis = currMillis;
+    }
   }
-
-
 
   /* ######################### Shift pressure detection and ignition Cut ######################### */
   // shiftQueued when threshold is reached and not on cooldown
@@ -273,8 +219,8 @@ void loop()
   if (currMillis >= (lastPulseMillis + rpmLoopTime))
   {
     noInterrupts();
-    //lastRPM = (pulseCount * 60 * ((1000 / rpmLoopTime)));
-    lastRPM = 7000;
+    lastRPM = (pulseCount * 60 * ((1000 / rpmLoopTime)));
+    //lastRPM = 6000;
     pulseCount = 0;
     interrupts();
 
