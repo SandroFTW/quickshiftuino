00775b729b9234b90844905bd7babc33bc000759
Sandro Hafner <34171311+SandroFTW@users.noreply.github.com>
2024-04-21 14:57:01 +0200

    Add Hall Gear Sensor
    First version of Hall compatibility. The sensor is mounted on the gear lever to detect when the gear is fully engaged. This eliminates the need for fixed cutoff times and should be way smoother.

--- src/src.ino
+++ src/src.ino
@@ -1,19 +1,26 @@
 // CHANGE CONSOLE DEBUG OUTPUT
 //#define DEBUG_SENSOR
 //#define DEBUG_SENSOR_PLOT
+//#define DEBUG_HALL_PLOT
 //#define DEBUG_SHIFT
 //#define DEBUG_RPM
+
+// Possible BUGS in Hall Mode: - Gear is not engaged anymore after 5ms time has passen -> false neutral
+//                             - Engine shuts off because gearSensitivity is never reached (always turn on ignition after cutSafeTime?)
+
+#define USE_HALL
 
 // QUICKSHIFTER CONFIGURATION
 #define loopTime         5    // 200 Hz
 #define rpmLoopTime      200  // 5 Hz
 
-#define cutTime1         60    // 3000 - 5375   RPM
-#define cutTime2         55    // 5376 - 7750   RPM
-#define cutTime3         55    // 7751 - 10125  RPM
-#define cutTime4         50    // 10126 - 12500 RPM
+#define cutTime1         85    // 3000 - 5375   RPM 60
+#define cutTime2         80    // 5376 - 7750   RPM 55
+#define cutTime3         75    // 7751 - 10125  RPM 55
+#define cutTime4         70    // 10126 - 12500 RPM 50
+#define cutSafeTime      150
 
-#define minRPM           3000  // min. required RPM to allow QS 
+#define minRPM           2500  // min. required RPM to allow QS    3000
 #define maxRPM           12500 // Engine RPM for minCutTime (still works above, always cutTimeMin)
 #define rpmStep          (maxRPM - minRPM) / 4
 
@@ -22,10 +29,15 @@
 #define rpmFactor        60 * ((1000 / rpmLoopTime))
 
 // SENSITIVITY VALUES
-#define cutSensitivity   240   // min. piezo force threshold
+#define cutSensitivity    220   // min. piezo force threshold
+#define cancelSensitivity -40   // threshold to detect downshift direction
+#define centerSensitivity 0     // value to sense gear lever in center position
+#define gearSensitivity   200   // threshold to sense gear fully engaged
+#define centerTolerance   30    // +/- tolerance for center position
 
 // PIN DEFINES
 #define PIEZO_SENSOR     17 // A3  (Piezo Sensor Pin) (A4/18; A5/19 broken)
+#define HALL_SENSOR      16 // A2  (Hall Sensor Pin)
 #define CUT_SIG          10 // D10, PB2 (Ignition cut MOSFET pin)
 #define GREEN_LED        3  // D3
 //#define RED_LED          2  // D2
@@ -36,6 +48,7 @@ static unsigned long lastPulseMillis = 0; // Time of last RPM calculation
 static unsigned long lastCutMillis = 0;   // Time of last shiftQueued
 static unsigned long currMillis = 0;      // millis()
 static unsigned long lastMillis = 0;      // Last cycle millis() (currMillis - loopTime)
+static unsigned long finishedMillis = 0;  // Time when hall sensor detected gear fully engaged
 static volatile int pulseCount = 0;       // Ignition interrupt count since last RPM calculation
 static bool ignitionDisabled = false;     // Is ignition currently disabled?
 static bool shiftCooldown = true;         // Is QS on cooldown after last shift?
@@ -44,29 +57,7 @@ static float cutTime = 0;                 // Calculated cutTime based on RPM
 static float lastRPM = 0;                 // Last calculated RPM
 
 static int sensorValue = 0;
-
-// CALCULATE CUTOFF TIME BASED ON ENGINE RPM
-/*
-int cutoffTime()
-{
-  if (lastRPM >= minRPM && lastRPM <= (minRPM + rpmStep))
-  {
-    return cutTime1;
-  }
-  else if (lastRPM > (minRPM + rpmStep) && lastRPM <= (minRPM + 2*rpmStep))
-  {
-    return cutTime2;
-  }
-  else if (lastRPM > (minRPM + 2*rpmStep) && lastRPM <= (minRPM + 3*rpmStep))
-  {
-    return cutTime3;
-  }
-  else if (lastRPM > (minRPM + 3*rpmStep))
-  {
-    return cutTime4;
-  }
-}
-*/
+static int hallValue = 0;
 
 int cutoffTime() // rpmstep = 2375
 {
@@ -117,16 +108,6 @@ void countPulse()
 
 void setup()
 {
-  //pinMode(GREEN_LED,    OUTPUT);
-  //pinMode(RED_LED,      OUTPUT);
-  //pinMode(CUT_SIG,      OUTPUT);
- 
-  //digitalWrite(GREEN_LED, LOW);
-  //digitalWrite(RED_LED,   LOW);
-  //digitalWrite(CUT_SIG,   HIGH);
-
-  //pinMode(PIEZO_SENSOR, INPUT);
-
   DDRD |= (1 << DDD3); // D3 (Green LED) as Output
   DDRB |= (1 << DDB2); // D10 (Ign Cut FET) as Output
 
@@ -134,6 +115,7 @@ void setup()
   PORTB |= (1 << DDB2); // Turn on Ignition
 
   DDRC &= ~(1 << DDC4); // A4 (Piezo) as Input
+  DDRC &= ~(1 << DDC2); // A2 (Hall) as Input
 
   Serial.begin(57600); // 115200
 
@@ -150,6 +132,7 @@ void loop()
   if (currMillis >= (lastMillis + loopTime))
   {
     sensorValue = analogRead(PIEZO_SENSOR) - 512;
+    hallValue = analogRead(HALL_SENSOR) - 512;
     lastMillis = currMillis;
 
     if (currMillis >= lastPrintMillis + 35)
@@ -160,6 +143,10 @@ void loop()
 
       #ifdef DEBUG_SENSOR_PLOT
         Serial.print("min:-512,max:512,sensorValue:"); Serial.println(sensorValue);
+      #endif
+
+      #ifdef DEBUG_HALL_PLOT
+        Serial.print("min:0,max:1023,hallValue:"); Serial.println(hallValue);
       #endif
 
       lastPrintMillis = currMillis;
@@ -199,6 +186,7 @@ void loop()
     if (sensorValue >= cutSensitivity)// Check if sensor still above threshold
     {
       disable_ign();
+      Serial.println("Ignition disabled.");
 
       cutTime = cutoffTime();
       ignitionDisabled = true;
@@ -219,6 +207,30 @@ void loop()
     }
   }
 
+#ifdef USE_HALL
+  // Use hall sensor value instead of fixed cutoff time
+  if ((hallValue >= gearSensitivity) && ignitionDisabled) // && currMillis >= (lastCutMillis + delayTime + 25)) 
+  {
+    finishedMillis = currMillis;
+  }
+
+  // Add 5ms of delay after hall sensor detected shift complete, just to be safe (or enable instantly when downshift is detected)
+  if (((finishedMillis >= (currMillis + 5)) || hallValue <= cancelSensitivity) && ignitionDisabled)
+  {
+    enable_ign();
+    ignitionDisabled = false;
+    //Serial.println("Ignition enabled - (hall sensor detected upshift complete");
+  }
+
+  // Reset cooldown after hall sensor detected shift lever in center position
+  if (sensorValue < cutSensitivity && shiftCooldown)
+  {
+    if ((hallValue <= (centerSensitivity + hallTolerance) && hallValue >= (centerSensitivity - hallTolerance)) || (currMillis >= (lastCutMillis + delayTime + cutSafeTime)))
+    {
+      shiftCooldown = false;
+    }
+  }
+#else
   // Enable Ignition when cutTime has passed
   if (currMillis >= (lastCutMillis + delayTime + cutTime) && ignitionDisabled)
   {
@@ -226,7 +238,7 @@ void loop()
     ignitionDisabled = false;
 
     #ifdef DEBUG_SHIFT
-      Serial.print(currMillis); Serial.println(" - Shift complete (Ignition Enabled)\n");
+      Serial.print(currMillis); Serial.println(" - Shift complete (Ignition Enabled, cutTime passed)\n");
     #endif
   }
 
@@ -235,6 +247,7 @@ void loop()
   {
     shiftCooldown = false;
   }
+#endif
 
 
 
