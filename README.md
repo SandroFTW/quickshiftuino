4066d10af7ff0460e3f772297436ee689ccbce9b
Sandro Hafner <34171311+SandroFTW@users.noreply.github.com>
2024-10-02 11:42:50 +0200

    R2 PCB files and latest code

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # quickshiftuino
+<<<<<<< Updated upstream
 ## Latest development (edited 21.08.2024)
 The new dimensions are 40 x 35 mm so it's also a LOT more compact now.
 Gerber files and schematics are already available but still work in progress
@@ -7,6 +8,17 @@ The second revision PCBs are currently being tested and seem to be working fairl
 I ordered some new tiny PCBs to hold a SOT23-3 hall sensor. The two stage ignition switching does a great job at reducing abrupt power engagement but it takes some tuning to get right. I'm hoping with the new hall sensor setup it will be much easier to get running perfectly smooth. Previous experiments have proven that this approach works well.
 
 <img src='img/r2_electronics.JPG' width='350'>
+=======
+## Latest development (edited 02.10.2024)
+The third revision (R3) PCB is done and ordered. I added Molex Picoblade connectors to the board for two hall sensor connections without soldering. The dimensions increased slightly from 35x40mm to 42x40mm.
+Vias are now also tented to prevent short circuits between Quickshiftuino PCB and the piggyback ESP32 Mini.
+Currently I'm developing a hall pressure sensor that mounts at one end of the shift rod. 3D printed prototypes already give promising results, aluminium CNC milled parts are going to arrive in 2 weeks.
+
+Gerber files will be available after I did some testing.
+
+Previous R2 PCB that's almost the same as R3:
+<img src='img/pcb_r2.png' width='350'>
+>>>>>>> Stashed changes
 
 <br>
 
@@ -15,35 +27,52 @@ Quickshiftuino is a simple device that detects the shift motion in the gear leve
 This enables clutchless upshifting (and in some situations downshifting) by unloading the gearbox and drivetrain.
 
 The cutoff time is dependant on engine RPM and is linearly scaled bewtween the lowest RPM (minRPM) and highest RPM (maxPRM).
+An alternative to fixed cut-times would be a hall sensor and magnet mounted to the gearbox shift shaft to detect once the gearbox dog rings are touching or fully engaged.
 
-My main idea was to keep it simple so it's as universal as possible. That's why I chose to use an Arduino Nano as the MCU for the first revision.
+My main idea was to keep it simple to use and universal. In total there are 6 possible configurations that have different advantages and disadvantages.
+
+Configuration is possible though a locally hosted web-interface. Simply connect to the WiFi network provided by the device.
+http://192.168.4.1/ gives access to the config page.
+http://192.168.4.1/debug.html/ shows output values of the selected sensors and the current RPM value. This is helpful for finetuning parameters.
 
 <br>
 
-The V1 PCB had a few issues with the power switching logic (high side P-MOS was wired incorrectly):
+The R1 PCB had a few issues with the power switching logic (high side P-MOS was wired incorrectly):
 
 <img src='img/r1_electronics.jpg' width='350'>
+
+For R2 many little bugs were resolved and the size drastically reduced.
+The MCU was also switched from an Arduino Nano to a much more powerful and compact ESP32 Mini (Waveshare).
 
 <br>
 
 ## Shift detection
 Shift lever pressure can be measured in two different ways with quickshiftuino.
 
-  ### 1. Load cell
+  ### 1. Piezo quickshifter sensor
+  These sensors are all around Aliexpress if you search for quickshifter parts. A few big companies are using something similar with success already.
+  This sensor doesn't react to force but it detects force change or momentum. The applied force can be more than enough but the sensor doesn't output a big voltage if you're shifting really soft and slow.
+  The main drawback is that it only gives reliable results when mounted on the gearbox shaft clamp. That way there won't be a signal difference between up- and downshifts though. Installation is probably the easiest out of all.
+  
+  ### 2. Hall Sensor pressure sensor
+  This is the latest option and still under development. Current tests show really good results and easy setup. The required parts are a bit pricier than the Piezo sensor though. A hall sensor measures the movement of a spring loaded mechanism that's mounted in the shift rod. Due to it being fairly low profile a modification of the shift rod might not be required on many bikes.
+
+  ### 3. Load cell (not supported yet)
   The PCB features an HX711 load cell amplifier circuit.
   A DYMH-103 load cell needs to be installed in the shift rod to sense actual force.
   This method is more complex since in most cases the shift rod needs to be cut and modified but you will also get more consistent results.
   Another benefit is that up- and downshifts are clearly distinguishable because of positive and negative readings.
 
-  ### 2. Piezo quickshifter sensor
-  These sensors are all around Aliexpress if you search for quickshifter parts. A few big companies are using something similar with success already (e.g. Healtech, Starlane).
-  This sensor doesn't react to force but it detects force change or momentum. The applied force can be more than enough but the sensor doesn't output a big voltage if you're shifting really soft and slow.
-  The main drawback is that it only gives reliable results when mounted on the gearbox shaft clamp. That way there won't be a signal difference between up- and downshifts though.
-  Installation is really easy compared to the loadcell and mounted correctly it's barely noticeable.
-
 <br>
 
 ## Future plans
+  ### Software ToDo's
+  - Add hysteresis for hall sensor pressure sensor to prevent continuous triggering when holding the shift lever up in top gear
+  - Instead of measuring ignition pulses in a fixed timespan: Set up timer that resets with each ignition pulse (RPM way more precise and faster update rate)
+  - Long term: Improve efficiency of config GUI code and move all non-quickshifter tasks to second MCU core (enables faster loop times, probably not necessary)
+  - Fun project: 2-step RPM limiter for launch control or just cool flames out of the exhaust (relies on quick and precise RPM measurement)
+  - Fun project: Add support for shift indicator LED using free GPIO pad on R3 PCB (could also be used for other practical indicators)
+  
 Currently I am working on the second revision of the PCB which will have a few major issues resolved (mainly power switching logic).
 I'm also thinking about either switching to an ESP32 MCU or adding a bluetooth module to the PCB. This should enable changing configuration values without the need for a computer and Arduino IDE.
 
--- src/src.ino
+++ src/src.ino
@@ -15,24 +15,27 @@ WebSocketsServer webSocket = WebSocketsServer(81);
 
 // PIN DEFINES
 #define GREEN_LED 5
-#define RED_LED 6
+#define RED_LED   6
 #define IGN_FET_1 1
 #define IGN_FET_2 2
 #define PIEZO_PIN 13
-#define RPM_PIN 7
+#define HALL1_PIN 10 // Hall Sensor 1 (Gearbox Position)
+#define HALL2_PIN 11 // Hall Sensor 2 (Shift pressure)
+#define RPM_PIN   7
 
 // GLOBAL VARIABLES
 static unsigned long lastPulseMillis = 0; // Time of last RPM calculation
 static unsigned long lastCutMillis = 0;   // Time of last ignition cut begin
 static unsigned long currMillis = 0;      // millis()
 static unsigned long lastMillis = 0;      // Last cycle millis() (currMillis - loopTime)
+static volatile bool redLED = false;      // Used to toggle red LED on RPM pulse
 static unsigned int coilsDisabled = 0;    // Is ignition currently disabled?
 static volatile int pulseCount = 0;       // Ignition interrupt count since last RPM calculation
 static int currRpmRange = 0;              // 0-4 to choose cutoffTime from array
 static float lastRPM = 0;                 // Last calculated RPM
-static volatile bool redled = false;      // Used to toggle red LED on RPM pulse
 
-static int sensorValue = 0;
+static float pressureValue = 0;
+static float gearboxValue = 0;
 
 String params = "["
   "{"
@@ -41,92 +44,118 @@ String params = "["
   "'type':"+String(INPUTPASSWORD)+","
   "'default':''"
   "},"
-  // "{"
-  // "'name':'opmode',"
-  // "'label':'Operation Mode',"
-  // "'type':"+String(INPUTSELECT)+","
-  // "'options':["
-  // "{'v':'PS','l':'Piezo Static'},"
-  // "{'v':'PH','l':'Piezo Hall'},"
-  // "{'v':'LS','l':'Loadcell Static'},"
-  // "{'v':'LH','l':'Loadcell Hall'}],"
-  // "'default':'PS'"
-  // "},"
   "{"
-  "'name':'cuttime1_1',"
-  "'label':'Cuttime 1 (2500-4500)',"
-  "'type':"+String(INPUTNUMBER)+","
-  "'min':20,'max':200,"
-  "'default':'62'"
+  "'name':'enable',"
+  "'label':'Enable Quickshiftuino',"
+  "'type':"+String(INPUTCHECKBOX)+","
+  "'default':'1'"
   "},"
   "{"
-  "'name':'cuttime1_2',"
-  "'label':'Cuttime 1 (4500-6500)',"
+  "'name':'trigmode',"
+  "'label':'Trigger Mode',"
+  "'type':"+String(INPUTSELECT)+","
+  "'options':["
+  "{'v':'piezo','l':'Piezo'},"
+  "{'v':'hall','l':'Hall'},"
+  "{'v':'loadcell','l':'Loadcell'}],"
+  "'default':'hall'"
+  "},"
+  "{"
+  "'name':'enablemode',"
+  "'label':'Enable Mode',"
+  "'type':"+String(INPUTSELECT)+","
+  "'options':["
+  "{'v':'static','l':'Static (Time)'},"
+  "{'v':'hall','l':'Hall'}],"
+  "'default':'hall'"
+  "},"
+  "{"
+  "'name':'invertpressure',"
+  "'label':'Invert Pressure Sensor',"
+  "'type':"+String(INPUTCHECKBOX)+","
+  "'default':'0'"
+  "},"
+  "{"
+  "'name':'invertgearbox',"
+  "'label':'Invert Gearbox Sensor',"
+  "'type':"+String(INPUTCHECKBOX)+","
+  "'default':'0'"
+  "},"
+  "{"
+  "'name':'cuttime1_1',"
+  "'label':'First Cut (2500-4500)',"
   "'type':"+String(INPUTNUMBER)+","
-  "'min':20,'max':200,"
+  "'min':0,'max':500,"
   "'default':'58'"
   "},"
   "{"
-  "'name':'cuttime1_3',"
-  "'label':'Cuttime 1 (6500-8500)',"
+  "'name':'cuttime1_2',"
+  "'label':'First Cut (4500-6500)',"
   "'type':"+String(INPUTNUMBER)+","
-  "'min':20,'max':200,"
-  "'default':'52'"
+  "'min':0,'max':500,"
+  "'default':'56'"
+  "},"
+  "{"
+  "'name':'cuttime1_3',"
+  "'label':'First Cut (6500-8500)',"
+  "'type':"+String(INPUTNUMBER)+","
+  "'min':0,'max':500,"
+  "'default':'54'"
   "},"
   "{"
   "'name':'cuttime1_4',"
-  "'label':'Cuttime 1 (8500-10500)',"
+  "'label':'First Cut (8500-10500)',"
   "'type':"+String(INPUTNUMBER)+","
-  "'min':20,'max':200,"
-  "'default':'50'"
+  "'min':0,'max':500,"
+  "'default':'52'"
   "},"
   "{"
   "'name':'cuttime1_5',"
-  "'label':'Cuttime 1 (10500-12500)',"
+  "'label':'First Cut (10500-12500)',"
   "'type':"+String(INPUTNUMBER)+","
-  "'min':20,'max':200,"
+  "'min':0,'max':500,"
   "'default':'50'"
   "},"
   "{"
   "'name':'cuttime2_1',"
-  "'label':'Cuttime 2 (2500-4500)',"
+  "'label':'Second Cut (2500-4500)',"
   "'type':"+String(INPUTNUMBER)+","
-  "'min':20,'max':200,"
+  "'min':0,'max':500,"
   "'default':'66'"
   "},"
   "{"
   "'name':'cuttime2_2',"
-  "'label':'Cuttime 2 (4500-6500)',"
+  "'label':'Second Cut (4500-6500)',"
   "'type':"+String(INPUTNUMBER)+","
-  "'min':20,'max':200,"
-  "'default':'62'"
+  "'min':0,'max':500,"
+  "'default':'64'"
   "},"
   "{"
   "'name':'cuttime2_3',"
-  "'label':'Cuttime 2 (6500-8500)',"
+  "'label':'Second Cut (6500-8500)',"
   "'type':"+String(INPUTNUMBER)+","
-  "'min':20,'max':200,"
-  "'default':'54'"
+  "'min':0,'max':500,"
+  "'default':'60'"
   "},"
   "{"
   "'name':'cuttime2_4',"
-  "'label':'Cuttime 2 (8500-10500)',"
+  "'label':'Second Cut (8500-10500)',"
   "'type':"+String(INPUTNUMBER)+","
-  "'min':20,'max':200,"
-  "'default':'54'"
+  "'min':0,'max':500,"
+  "'default':'56'"
   "},"
   "{"
   "'name':'cuttime2_5',"
-  "'label':'Cuttime 2 (10500-12500)',"
+  "'label':'Second Cut (10500-12500)',"
   "'type':"+String(INPUTNUMBER)+","
-  "'min':20,'max':200,"
-  "'default':'52'"
+  "'min':0,'max':500,"
+  "'default':'54'"
   "},"
   "{"
   "'name':'minrpm',"
   "'label':'Min RPM',"
   "'type':"+String(INPUTNUMBER)+","
-  "'min':1000,'max':20000,"
+  "'min':100,'max':20000,"
   "'default':'2500'"
   "},"
   "{"
@@ -144,17 +173,45 @@ String params = "["
   "},"
   "{"
   "'name':'deadtime',"
-  "'label':'Dead time',"
+  "'label':'Dead time (ms)',"
   "'type':"+String(INPUTNUMBER)+","
-  "'min':0,'max':1000,"
+  "'min':0,'max':5000,"
   "'default':'350'"
   "},"
   "{"
   "'name':'cutsensitivity',"
-  "'label':'Cut sensitivity',"
+  "'label':'Cut sensitivity (0-100)',"
   "'type':"+String(INPUTNUMBER)+","
   "'min':0,'max':2000,"
   "'default':'250'"
+  "},"
+  "{"
+  "'name':'halltouching',"
+  "'label':'Hall dogs touching (0-100)',"
+  "'type':"+String(INPUTNUMBER)+","
+  "'min':0,'max':20000,"
+  "'default':'2000'"
+  "},"
+  "{"
+  "'name':'hallengaged',"
+  "'label':'Hall gear engaged (0-100)',"
+  "'type':"+String(INPUTNUMBER)+","
+  "'min':0,'max':20000,"
+  "'default':'3000'"
+  "},"
+  "{"
+  "'name':'twosteprpm',"
+  "'label':'2 Step RPM (0=OFF)',"
+  "'type':"+String(INPUTNUMBER)+","
+  "'min':0,'max':20000,"
+  "'default':'0'"
+  "},"
+  "{"
+  "'name':'twostephyst',"
+  "'label':'2 Step Hyst',"
+  "'type':"+String(INPUTNUMBER)+","
+  "'min':0,'max':5000,"
+  "'default':'200'"
   "}"
   "]";
 
@@ -166,7 +223,13 @@ struct cfgOptions
   char ssid[32];
   char password[32];
 
-  char opmode[4];      // PS, PH, LS, LH
+  bool enable;
+
+  char trigMode[3];      // piezo, hall, loadcell
+  char enableMode[2];    // static, hall
+
+  bool invertPressure;
+  bool invertGearbox;
 
   int cutTime1[5]; // ms
   int cutTime2[5]; // ms
@@ -178,13 +241,24 @@ struct cfgOptions
   int deadTime;        // ms
 
   int cutSensitivity;
+  int hallTouching;
+  int hallEngaged;
+
+  int twoStepRPM;
+  int twoStepHyst;
 } cfg;
 
 
 
 void transferWebconfToStruct(String results)
 {
-    //cfg.opmode = conf.getString("opmode");
+    cfg.enable = conf.getBool("enable");
+
+    cfg.trigMode = conf.getString("trigmode");
+    cfg.enableMode = conf.getString("enablemode");
+
+    cfg.invertPressure = conf.getBool("invertpressure");
+    cfg.invertGearbox = conf.getBool("invertgearbox");
 
     cfg.cutTime1[0] = conf.getInt("cuttime1_1");
     cfg.cutTime1[1] = conf.getInt("cuttime1_2");
@@ -205,6 +279,11 @@ void transferWebconfToStruct(String results)
     cfg.deadTime = conf.getInt("deadtime");
 
     cfg.cutSensitivity = conf.getInt("cutsensitivity");
+    cfg.hallTouching = conf.getInt("halltouching");
+    cfg.hallEngaged = conf.getInt("hallengaged");
+
+    cfg.twoStepRPM = conf.getInt("twosteprpm");
+    cfg.twoStepHyst = conf.getInt("twostephyst");
 }
 
 void handleRoot(AsyncWebServerRequest *request)
@@ -223,7 +302,7 @@ void push_debug(void *parameters)
 {
   for (;;)
   {
-    String broadcastString = String(sensorValue) + "," + String(lastRPM);
+    String broadcastString = String(sensorValue) + "," + String(lastRPM) + "," + String(hallValue);
 
     webSocket.loop();
     webSocket.broadcastTXT(broadcastString);
@@ -253,8 +332,8 @@ void enable_ign_2()
 void countPulse()
 {
   pulseCount++;
-  digitalWrite(RED_LED, (redled ? HIGH : LOW));
-  redled = !redled;
+  digitalWrite(RED_LED, (redLED ? HIGH : LOW));
+  redLED = !redLED;
 }
 
 
@@ -268,11 +347,12 @@ void setup()
 
   digitalWrite(GREEN_LED, HIGH);
   digitalWrite(RED_LED, HIGH);
-
   digitalWrite(IGN_FET_1, HIGH);
   digitalWrite(IGN_FET_2, HIGH);
 
   pinMode(PIEZO_PIN, INPUT); // Piezo Sensor
+  pinMode(HALL1_PIN, INPUT); // Hall Sensor 1 (Gearbox Position)
+  pinMode(HALL2_PIN, INPUT); // Hall Sensor 2 (Shift Pressure)
 
   // Interrupt routine for ingition pulse counting / RPM calculation
   pinMode(RPM_PIN, INPUT_PULLUP);
@@ -306,14 +386,32 @@ void loop()
 
   if (currMillis >= (lastMillis + loopTime))
   {
-    sensorValue = (analogRead(PIEZO_PIN) - 2048) * (-1);
+    switch (cfg.trigMode)
+    {
+      case "piezo":
+        pressureValue = analogRead(PIEZO_PIN) / 40.95f;
+        break;
+
+      case "hall":
+        pressureValue = analogRead(HALL2_PIN) / 40.95f;
+        break;
+
+      case "loadcell":
+        pressureValue = 0.f;
+        break;
+    }
+
+    gearboxValue = analogRead(HALL1_PIN) / 40.95f;
+
+    if (cfg.invertPressure) pressureValue = 100.f - pressureValue;
+    if (cfg.invertGearbox) gearboxValue = 100.f - gearboxValue;
 
     lastMillis = currMillis;
   }
 
   /* ######################### Shift pressure detection and ignition cut ######################### */
   // Disable ignition when pressure is sensed and sufficient RPM
-  if (lastRPM >= cfg.minRPM && coilsDisabled == 0 && sensorValue >= cfg.cutSensitivity && currMillis >= (lastCutMillis + cfg.deadTime))
+  if (lastRPM >= cfg.minRPM && coilsDisabled == 0 && pressureValue >= cfg.cutSensitivity && currMillis >= (lastCutMillis + cfg.deadTime))
   {
       disable_ign();
       coilsDisabled = 2;
@@ -323,20 +421,56 @@ void loop()
       lastCutMillis = currMillis;
   }
 
-  // Enable ignition 1 when dog rings touching
-  //if (coilsDisabled == 2 && currMillis >= (lastCutMillis + 50))
-  if (coilsDisabled == 2 && currMillis >= (lastCutMillis + cfg.cutTime1[currRpmRange]))
+  // Enable ignition 1 when dog rings touching or time 1 passed
+  if (coilsDisabled == 2)
+  {
+    if ((cfg.enableMode == "static" && currMillis >= (lastCutMillis + cfg.cutTime1[currRpmRange])) ||
+        (cfg.enableMode == "hall" && hallValue >= cfg.hallTouching))
+    {
+      enable_ign_1();
+      coilsDisabled = 1;
+    }
+  }
+
+  // Enable ignition 2 when gear fully engaged or time 2 passed
+  if (coilsDisabled == 1)
+  {
+    if ((cfg.enableMode == "static" && currMillis >= (lastCutMillis + cfg.cutTime2[currRpmRange])) ||
+        (cfg.enableMode == "hall" && hallValue >= cfg.hallEngaged))
+    {
+      enable_ign_2();
+      coilsDisabled = 0;
+    }
+  }
+
+  //Safety feature to prevent engine shutting off in case of an issue
+  if (coilsDisabled == 2 && currMillis >= (lastCutMillis + 150))
   {
     enable_ign_1();
     coilsDisabled = 1;
   }
-
-  // Enable ignition 2 when gear fully engaged
-  //if (coilsDisabled == 1 && currMillis >= (lastCutMillis + 55))
-  if (coilsDisabled == 1 && currMillis >= (lastCutMillis + cfg.cutTime2[currRpmRange]))
+  if (coilsDisabled == 1 && currMillis >= (lastCutMillis + 160))
   {
     enable_ign_2();
     coilsDisabled = 0;
+  }
+
+
+  /* ######################### Two Step Rev Limiter ######################### */
+  // (just for fun, WIP)
+  if (cfg.twoStepRPM > 0)
+  {
+    if (lastRPM > cfg.twoStepRPM)
+    {
+      disable_ign();
+      coilsDisabled = 2;
+    }
+    else if (lastRPM < (cfg.twoStepRPM - cfg.twoStepHyst))
+    {
+      enable_ign_1();
+      enable_ign_2();
+      coilsDisabled = 0;
+    }
   }
 
 
